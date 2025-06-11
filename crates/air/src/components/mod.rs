1460cc05558c5a71f55ce6c1485c945f46c7569f
Clément Walter <clement0walter@gmail.com>
2025-06-11 16:58:40 +0200

    Scaffolding

--- crates/air/src/components/mod.rs
+++ crates/air/src/components/mod.rs
@@ -1,10 +1,12 @@
 pub mod multiple_constraints;
 pub mod single_constraint;
+pub mod single_constraint_with_relation;
 
+use num_traits::Zero;
 pub use stwo_air_utils::trace::component_trace::ComponentTrace;
 pub use stwo_air_utils_derive::{IterMut, ParIterMut, Uninitialized};
-use stwo_prover::core::air::Component as ComponentVerifier;
 pub use stwo_prover::core::backend::simd::m31::PackedM31;
+use stwo_prover::core::{air::Component as ComponentVerifier, fields::qm31::QM31};
 use stwo_prover::{
     constraint_framework::TraceLocationAllocator,
     core::{
@@ -17,9 +19,24 @@ use stwo_prover::{
     },
 };
 
+use crate::relations;
+
 pub struct Claim<const N: usize> {
     pub single_constraint: single_constraint::Claim<N>,
     pub multiple_constraints: multiple_constraints::Claim<N>,
+    pub single_constraint_with_relation: single_constraint_with_relation::Claim<N>,
+}
+
+pub struct LookupData {
+    pub single_constraint_with_relation: single_constraint_with_relation::LookupData,
+}
+
+pub struct Relations {
+    pub memory: relations::Memory,
+}
+
+pub struct InteractionClaim {
+    pub single_constraint_with_relation: single_constraint_with_relation::InteractionClaim,
 }
 
 impl<const N: usize> Claim<N> {
@@ -27,6 +44,7 @@ impl<const N: usize> Claim<N> {
         Self {
             single_constraint: single_constraint::Claim::new(log_size),
             multiple_constraints: multiple_constraints::Claim::new(log_size),
+            single_constraint_with_relation: single_constraint_with_relation::Claim::new(log_size),
         }
     }
 
@@ -34,6 +52,7 @@ impl<const N: usize> Claim<N> {
         let trees = vec![
             self.single_constraint.log_sizes(),
             self.multiple_constraints.log_sizes(),
+            self.single_constraint_with_relation.log_sizes(),
         ];
         TreeVec::concat_cols(trees.into_iter())
     }
@@ -41,27 +60,95 @@ impl<const N: usize> Claim<N> {
     pub fn mix_into(&self, channel: &mut impl Channel) {
         self.single_constraint.mix_into(channel);
         self.multiple_constraints.mix_into(channel);
+        self.single_constraint_with_relation.mix_into(channel);
     }
 
     pub fn write_trace<MC: MerkleChannel>(
         &self,
-    ) -> impl IntoIterator<Item = CircleEvaluation<SimdBackend, M31, BitReversedOrder>>
+    ) -> (
+        impl IntoIterator<Item = CircleEvaluation<SimdBackend, M31, BitReversedOrder>>,
+        LookupData,
+    )
     where
         SimdBackend: BackendForChannel<MC>,
     {
-        let single_trace = self.single_constraint.write_trace().to_evals();
-        let multiple_trace = self.multiple_constraints.write_trace().to_evals();
-        [single_trace, multiple_trace].into_iter().flatten()
+        let single_trace = self.single_constraint.write_trace();
+        let multiple_trace = self.multiple_constraints.write_trace();
+        let (single_constraint_with_relation_trace, lookup_data) =
+            self.single_constraint_with_relation.write_trace();
+        (
+            [
+                single_trace.to_evals(),
+                multiple_trace.to_evals(),
+                single_constraint_with_relation_trace.to_evals(),
+            ]
+            .into_iter()
+            .flatten(),
+            LookupData {
+                single_constraint_with_relation: lookup_data,
+            },
+        )
+    }
+}
+
+impl InteractionClaim {
+    pub fn write_interaction_trace(
+        relations: &Relations,
+        lookup_data: &LookupData,
+    ) -> (
+        impl IntoIterator<Item = CircleEvaluation<SimdBackend, M31, BitReversedOrder>>,
+        InteractionClaim,
+    ) {
+        let (
+            single_constraint_with_relation_trace,
+            single_constraint_with_relation_trace_claimed_sum,
+        ) = single_constraint_with_relation::InteractionClaim::write_interaction_trace(
+            &relations.memory,
+            &lookup_data.single_constraint_with_relation,
+        );
+
+        (
+            [single_constraint_with_relation_trace]
+                .into_iter()
+                .flatten(),
+            InteractionClaim {
+                single_constraint_with_relation: single_constraint_with_relation_trace_claimed_sum,
+            },
+        )
+    }
+
+    pub fn claimed_sum(&self) -> SecureField {
+        let mut sum = QM31::zero();
+        sum += self.single_constraint_with_relation.claimed_sum;
+        sum
+    }
+
+    pub fn mix_into(&self, channel: &mut impl Channel) {
+        self.single_constraint_with_relation.mix_into(channel);
+    }
+}
+
+impl Relations {
+    pub fn draw(channel: &mut impl Channel) -> Self {
+        Self {
+            memory: relations::Memory::draw(channel),
+        }
     }
 }
 
 pub struct Components<const N: usize> {
     pub single_constraint: single_constraint::Component<N>,
     pub multiple_constraints: multiple_constraints::Component<N>,
+    pub single_constraint_with_relation: single_constraint_with_relation::Component<N>,
 }
 
 impl<const N: usize> Components<N> {
-    pub fn new(location_allocator: &mut TraceLocationAllocator, claim: &Claim<N>) -> Self {
+    pub fn new(
+        location_allocator: &mut TraceLocationAllocator,
+        claim: &Claim<N>,
+        interaction_claim: &InteractionClaim,
+        relations: &Relations,
+    ) -> Self {
         Self {
             single_constraint: single_constraint::Component::new(
                 location_allocator,
@@ -77,14 +164,32 @@ impl<const N: usize> Components<N> {
                 },
                 SecureField::default(),
             ),
+            single_constraint_with_relation: single_constraint_with_relation::Component::new(
+                location_allocator,
+                single_constraint_with_relation::Eval {
+                    claim: claim.single_constraint_with_relation,
+                    memory: relations.memory.clone(),
+                },
+                interaction_claim
+                    .single_constraint_with_relation
+                    .claimed_sum,
+            ),
         }
     }
 
     pub fn provers(&self) -> Vec<&dyn ComponentProver<SimdBackend>> {
-        vec![&self.single_constraint, &self.multiple_constraints]
+        vec![
+            &self.single_constraint,
+            &self.multiple_constraints,
+            &self.single_constraint_with_relation,
+        ]
     }
 
     pub fn verifiers(&self) -> Vec<&dyn ComponentVerifier> {
-        vec![&self.single_constraint, &self.multiple_constraints]
+        vec![
+            &self.single_constraint,
+            &self.multiple_constraints,
+            &self.single_constraint_with_relation,
+        ]
     }
 }
